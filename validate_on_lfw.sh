1af94896d60924cc95bb193cc64ce5f89ddfe06f
Peter Tanugraha <p.tanugraha@gmail.com>
2018-08-29 21:18:29 -0400

    Added bash scripts for validating model and aligning images

---
+++ validate_on_lfw.sh
@@ -0,0 +1,7 @@
+python src/validate_on_lfw.py \
+/Users/petertanugraha/Projects/facenet/datasets/lfw/lfw_mtcnnpy_160 \
+/Users/petertanugraha/Projects/facenet/trained_models/20180402-114759 \
+--distance_metric 1 \
+--use_flipped_images \
+--subtract_mean \
+--use_fixed_image_standardization
